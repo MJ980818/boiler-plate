be79f2c0d04745c2a6d93154e8710374903c4802
MJ98 <limminjeong98@naver.com>
2020-11-17 08:22:18 +0900

    privacy information deleted from development project

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key')
+
+const { User } = require("./models/User");
+
+//application/x-zzz-form-encoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application.json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://minjeong:alsrud327^^@boilerplate.4f2ze.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+// const { json } = require('body-parser')
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.get('/', (req, res) => res.send('Hello World nodemon'))
+
+app.post('/register', (req,res) => {
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success:false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Exmaple app listening on port ${port}!`))
